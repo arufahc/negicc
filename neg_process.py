59e3eec6645488e3277d7d8760489c743a4aa3b4
Alpha Lam <arufa.hc@gmail.com>
2023-12-23 20:25:08 -0800

    neg_process.py supports select best ICC profile to match the exposure of the scan. Also added support for multi-shot.

--- neg_process.py
+++ neg_process.py
@@ -14,9 +14,11 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
+import math
 import os
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 
 parser = argparse.ArgumentParser(
@@ -25,22 +27,34 @@ parser.add_argument(
     "--emulsion", '-e',
     choices=[
         'ektar100',
+        'portra160',
+        'portra400',
+    ],
+    help="Emulsion of the scanned film. Profile will be selected automatically.")
+parser.add_argument(
+    "--profile", '-p',
+    choices=[
+        'ektar100',
         'ektar100-1',
         'ektar100+1',
         'ektar100+2',
-        'portra400',
-        'portra400-1',
-        'portra400+1',
-        'portra400+2',
         'portra160',
         'portra160-1',
         'portra160+1',
         'portra160+2',
+        'portra400',
+        'portra400-1',
+        'portra400+1',
+        'portra400+2',
     ],
-    help="Emulsion of the scanned film or the name of the generated profile.")
+    help="Profile of the scanned film or the name of the generated profile.")
 parser.add_argument(
     '--raw_file', '-f',
     help="Name of the input raw file.")
+parser.add_argument(
+    '--multi_shot', '-M',
+    action='store_true',
+    help="Sony 4-shots multishot mode. Assume 4 consecutive shots from [raw_file]")
 parser.add_argument(
     '--measurement', '-m',
     choices=['', 'R190808'],
@@ -60,20 +74,60 @@ if args.target_mode:
                     '-o', Path(args.raw_file).stem + ('.target.tif'),
                     args.raw_file], check=True)
     exit(0)
-matrix = []
-with open('%s/icc_out/Sony A7RM4 %s %s CC Matrix.txt' % (os.path.dirname(__file__),
-                                                         args.emulsion.capitalize(),
-                                                         args.measurement)) as f:
-    for i in range(0, 3):
-        matrix.append(f.readline().strip('\r\n'))
 
-subprocess.run([os.path.join(os.path.dirname(__file__), 'bin_out', 'neg_process'),
-                '-r', matrix[0],
-                '-g', matrix[1],
-                '-b', matrix[2],
-                '-q', str(args.quality),
-                '-p', '%s/icc_out/Sony A7RM4 %s %s cLUT.icc' % (os.path.dirname(__file__),
-                                                                args.emulsion.capitalize(),
-                                                                args.measurement),
-                '-o', Path(args.raw_file).stem + ('.%s.tif' % args.emulsion),
-                args.raw_file], check=True)
+def read_profile_info(name):
+    matrix = []
+    shutter_speed = ''
+    with open('%s/icc_out/Sony A7RM4 %s %s Info.txt' % (os.path.dirname(__file__),
+                                                        name.capitalize(),
+                                                        args.measurement)) as f:
+        for i in range(0, 3):
+            matrix.append(f.readline().strip('\r\n'))
+        shutter_speed = f.readline().split(' ')[0]
+    return {'name': name, 'matrix': matrix, 'shutter_speed': float(shutter_speed)}
+
+profile = {}
+
+# If profile is not specified use emulsion and shutter speed to select profile automatically.
+if not args.profile:
+    if not args.emulsion:
+        print('At least --emulsion needs to be specified!')
+        exit(1)
+    profiles = []
+    profiles.append(read_profile_info(args.emulsion))
+    profiles.append(read_profile_info(args.emulsion + '-1'))
+    profiles.append(read_profile_info(args.emulsion + '+1'))
+    profiles.append(read_profile_info(args.emulsion + '+2'))
+    raw_shutter_speed = subprocess.check_output([os.path.join(os.path.dirname(__file__), 'bin_out', 'raw_info'),
+                                                 '-s', args.raw_file]).decode(sys.stdout.encoding).strip()
+    raw_shutter_speed = float(raw_shutter_speed.strip('\r\n'))
+    print("Raw shutter speed: %f" % raw_shutter_speed)
+
+    max_exp_diff = 100
+    for p in profiles:
+        exp_diff = abs(math.log2(p['shutter_speed'] / raw_shutter_speed))
+        if exp_diff < max_exp_diff:
+            max_exp_diff = exp_diff
+            profile = p
+    print("Choose profile %s with shutter speed: %f" % (profile['name'], profile['shutter_speed']))
+else:
+    profile = read_profile_info(args.profile)
+
+neg_process_args = [
+    os.path.join(os.path.dirname(__file__), 'bin_out', 'neg_process'),
+    '-r', profile['matrix'][0],
+    '-g', profile['matrix'][1],
+    '-b', profile['matrix'][2],
+    '-q', str(args.quality),
+    '-p', '%s/icc_out/Sony A7RM4 %s %s cLUT.icc' % (os.path.dirname(__file__),
+                                                    profile['name'].capitalize(),
+                                                    args.measurement),
+    '-o', Path(args.raw_file).stem + ('.%s.tif' % profile['name']),
+    args.raw_file]
+
+if args.multi_shot:
+    file_num = int(Path(args.raw_file).stem[-4:])
+    for i in range(1,4):
+        neg_process_args.append(Path(args.raw_file).stem[0:-4] + str(file_num + i) + Path(args.raw_file).suffix)
+
+subprocess.run(neg_process_args, check=True)
